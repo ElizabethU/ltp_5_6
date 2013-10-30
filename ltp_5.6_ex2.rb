469e2c25f899939be03cf21048841e145aabb9f2
Elizabeth Uselton <elizabeth.uselton@gmail.com>
2013-10-29 22:06:31 -0700

    Hopefully this has the files

---
+++ ltp_5.6_ex2.rb
@@ -0,0 +1,4 @@
+puts "What's your favorite number?"
+fav_num = gets.chomp
+new_fav = fav_num.to_i + 1
+puts "Yeah, #{fav_num} is pretty nice, but what about #{new_fav}? I mean, it's almost the same, but it can't hurt to have an extra 1, just in case, right?"
